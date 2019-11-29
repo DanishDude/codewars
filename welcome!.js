3471033760d24899ab496aa5da3808426156aec3
Christian Hinge <christianhinge@iMac-de-Christian.local>
2019-11-29 16:10:38 +0100

    vowel count

---
+++ welcome!.js
@@ -0,0 +1,28 @@
+
+function greet(language) {
+    const lang = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+
+    return Object.keys(lang).includes(language) ? lang.language : lang.english;
+}
+
+console.log(greet('english'));
+console.log(greet('dutch'));
+console.log(greet('IP_ADDRESS_INVALID'));
